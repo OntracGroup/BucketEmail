4f8425ec4862bdfce3adc3a0aa94af1877601a80
OntracGroup <hayden.l@ontracgroup.com.au>
2024-12-11 09:28:18 +1100

    Update BucketEmail.py

--- BucketEmail.py
+++ BucketEmail.py
@@ -882,7 +882,7 @@ if st.session_state.calculate_button:
             
             # Ask for email after successful calculation
             if sheet:  # Ensure the sheet is connected
-                st.subheading('Would you like a side-by-side comparison sent to your email?')
+                st.subheader('Would you like a side-by-side comparison sent to your email?')
                 collect_email(sheet, user_data, optimal_bucket, comparison_df)
         else:
             st.warning("No suitable bucket found within SWL limits.")
